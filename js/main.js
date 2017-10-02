a8bc35c113b4fb803dfa3f80455ab28e779cc172
ivan8830 <ivan8830@yandex.ru>
2017-10-02 10:23:21 +0500

    Работа над ошибками 2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
 
 const templates = [...document.querySelectorAll(`template`)];
-let i = 0;
+const mainCentral = document.querySelector(`.central`);
 
 const keyRight = 39;
 const keyLeft = 37;
-let mainCentral = document.querySelector(`.central`);
-let renderScreen = function (n) {
+
+let i = 0;
+
+const renderScreen = function (n) {
   mainCentral.innerHTML = templates[n].innerHTML;
 };
 
 renderScreen(i);
 
 document.addEventListener(`keydown`, function (e) {
-  if (e.altKey && e.keyCode === keyRight) {
-    if (i < templates.length) {
-      renderScreen(i + 1);
-      i++;
-    }
+  if (e.altKey && e.keyCode === keyRight && i < templates.length) {
+    renderScreen(i++);
   }
-  if (e.altKey && e.keyCode === keyLeft) {
-    if (i > 0) {
-      renderScreen(i - 1);
-      i--;
-    }
+  if (e.altKey && e.keyCode === keyLeft && i > 0) {
+    renderScreen(i--);
   }
 });
