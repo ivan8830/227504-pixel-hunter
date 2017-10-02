852dc0620f5ae38f98113355bbc257f67623e50b
ivan8830 <ivan8830@yandex.ru>
2017-10-02 10:10:51 +0500

    Работа над ошибками

--- js/main.js
+++ js/main.js
@@ -4,48 +4,23 @@ let i = 0;
 
 const keyRight = 39;
 const keyLeft = 37;
-const screen = function (n) {
-  let template = templates[n].innerHTML;
-  return template;
+let mainCentral = document.querySelector(`.central`);
+let renderScreen = function (n) {
+  mainCentral.innerHTML = templates[n].innerHTML;
 };
 
-let mainCentral = document.querySelector(`.central`);
-mainCentral.innerHTML = screen(i);
-
-/*document.addEventListener(`keydown`, function (evt) {
-  if (evt.keyCode === keyAlt) {
-    if (evt.keyCode === keyRight) {
-      mainCentral.innerHTML = screen(i + 1);
-    }
-    if (evt.keyCode === keyLeft) {
-      mainCentral.innerHTML = screen(i - 1);
-    }
-  }
-   if (evt.keyCode === keyAlt && keyRight) {
-   mainCentral.innerHTML = screen(i + 1);
- }
- if (evt.keyCode === keyAlt && keyLeft) {
-   mainCentral.innerHTML = screen(i - 1);
- }
-});*/
+renderScreen(i);
 
 document.addEventListener(`keydown`, function (e) {
-  e = e || window.event;
-  let b = i;
-  debugger;
   if (e.altKey && e.keyCode === keyRight) {
-    if (b === 6) {
-      mainCentral.innerHTML = screen(b);
-    } else {
-      mainCentral.innerHTML = screen(b + 1);
+    if (i < templates.length) {
+      renderScreen(i + 1);
       i++;
     }
   }
   if (e.altKey && e.keyCode === keyLeft) {
-    if (b === 0) {
-      mainCentral.innerHTML = screen(b);
-    } else {
-      mainCentral.innerHTML = screen(b - 1);
+    if (i > 0) {
+      renderScreen(i - 1);
       i--;
     }
   }
