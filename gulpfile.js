ceee38d35887ab7b6f6812425d27f5b0ee648c6a
ivan8830 <ivan8830@yandex.ru>
2017-10-10 20:13:34 +0500

    Настройка

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,16 @@ const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 const rollup = require('gulp-better-rollup');
 const sourcemaps = require('gulp-sourcemaps');
+const mocha = require('gulp-mocha');
+
+gulp.task('test', function () {
+  return gulp
+    .src(['js/**/*.test.js'], {read: false})
+    .pipe(mocha({
+      compilers: ['js:babel-register'],
+      reporter: 'spec'
+    }));
+});
 
 gulp.task('style', function () {
   return gulp.src('sass/style.scss')
